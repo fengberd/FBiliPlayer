35d9cc3b3af347fc715b7257d4273ecc0bbf3b47
fengberd <fengberd@gmail.com>
2016-09-15 18:34:35 +0800

    Init

---
+++ FBiliPlayer/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FBiliPlayer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.AutoSize = false;
+			this.trackBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.trackBar1.Location = new System.Drawing.Point(0, 412);
+			this.trackBar1.Maximum = 0;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(599, 19);
+			this.trackBar1.TabIndex = 1;
+			this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+			this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+			this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 10;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// vlcControl1
+			// 
+			this.vlcControl1.BackColor = System.Drawing.Color.Black;
+			this.vlcControl1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.vlcControl1.Location = new System.Drawing.Point(0, 0);
+			this.vlcControl1.Name = "vlcControl1";
+			this.vlcControl1.Size = new System.Drawing.Size(599, 406);
+			this.vlcControl1.Spu = -1;
+			this.vlcControl1.TabIndex = 2;
+			this.vlcControl1.VlcLibDirectory = null;
+			this.vlcControl1.VlcMediaplayerOptions = null;
+			this.vlcControl1.VlcLibDirectoryNeeded += new System.EventHandler<Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs>(this.vlcControl1_VlcLibDirectoryNeeded);
+			this.vlcControl1.Playing += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPlayingEventArgs>(this.vlcControl1_Playing);
+			this.vlcControl1.PositionChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPositionChangedEventArgs>(this.vlcControl1_PositionChanged);
+			this.vlcControl1.Stopped += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs>(this.vlcControl1_Stopped);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(273, 36);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(599, 431);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.vlcControl1);
+			this.Controls.Add(this.trackBar1);
+			this.Name = "MainForm";
+			this.Text = "Bilibili Player";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Timer timer1;
+		private Vlc.DotNet.Forms.VlcControl vlcControl1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ FBiliPlayer/MainForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using fastJSON;
+
+namespace FBiliPlayer
+{
+	public partial class MainForm : Form
+	{
+		private int playIndex = -1;
+		private bool lockTrackbar = false;
+		private long max_length = 0, current_offset = 0, current_length = 0;
+
+		private IList<string> playList = new List<string>();
+		private IDictionary<int,long[]> segment_table = new Dictionary<int,long[]>();
+
+		public MainForm()
+		{
+			InitializeComponent();
+			OnSizeChanged(null);
+		}
+
+		private bool PlayNext(int increase = 1)
+		{
+			if(playList.Count > (playIndex = playIndex + increase))
+			{
+				vlcControl1.Play(new Uri(playList[playIndex]));
+				current_offset = playIndex == 0 ? 0 : segment_table[playIndex - 1][0];
+				current_length = segment_table[playIndex][1];
+				Console.WriteLine("Playing:" + playList[playIndex]);
+				return true;
+			}
+			playIndex = -1;
+			vlcControl1.Stop();
+			return false;
+		}
+
+		private void PlayByIndexFolder(string path)
+		{
+			path = path.Replace("\\","/");
+			if(path[path.Length - 1] != '/')
+			{
+				path += "/";
+			}
+			if(!Directory.Exists(path) || !File.Exists(path + "index.json"))
+			{
+				throw new Exception("Path not exists or invalid.");
+			}
+			dynamic json = JSON.Parse(File.ReadAllText(path + "index.json"));
+			IList<object> segment_list = json["segment_list"];
+			playList.Clear();
+			segment_table.Clear();
+			vlcControl1.Stop();
+			for(int i = 0;i < segment_list.Count;i++)
+			{
+				IDictionary<string,object> segment = (IDictionary<string,object>)segment_list[i];
+				segment_table.Add(i,new long[]
+				{
+					(segment_table.Count == 0 ? 0 : segment_table[segment_table.Count - 1][0]) + (long)segment["duration"],
+					(long)segment["duration"]
+				});
+				playList.Add(path + i + ".mp4");
+			}
+			max_length = segment_table[segment_table.Count - 1][0];
+			playIndex = -1;
+			trackBar1.Value = 0;
+			trackBar1.Maximum = (int)(max_length / 1);
+			PlayNext();
+		}
+
+		private void timer1_Tick(object sender,EventArgs e)
+		{
+			if(vlcControl1.State == Vlc.DotNet.Core.Interops.Signatures.MediaStates.Ended && playIndex != -1)
+			{
+				PlayNext();
+			}
+		}
+
+		private void MainForm_Load(object sender,EventArgs e)
+		{
+			PlayByIndexFolder(@"R:\30216\lua.mp4.bapi.9");
+		}
+
+		private void MainForm_SizeChanged(object sender,EventArgs e)
+		{
+			vlcControl1.Height = ClientSize.Height - trackBar1.Height;
+		}
+
+		private void trackBar1_MouseDown(object sender,MouseEventArgs e)
+		{
+			lockTrackbar = true;
+		}
+
+		private void trackBar1_MouseUp(object sender,MouseEventArgs e)
+		{
+			long equal = trackBar1.Value * 1;
+			for(int i = 0;i < segment_table.Count;i++)
+			{
+				if(segment_table[i][0] > equal)
+				{
+					if(i != 0)
+					{
+						equal -= segment_table[i - 1][0];
+					}
+					if(i != playIndex || !vlcControl1.IsPlaying)
+					{
+						playIndex = i;
+						PlayNext(0);
+					}
+					vlcControl1.Position = equal / (float)segment_table[i][1];
+					lockTrackbar = false;
+					return;
+				}
+			}
+			throw new Exception("Video index out of range");
+		}
+
+		private void vlcControl1_Playing(object sender,Vlc.DotNet.Core.VlcMediaPlayerPlayingEventArgs e)
+		{
+			Invoke(new Action(delegate
+			{
+				timer1.Enabled = true;
+			}));
+		}
+
+		private void vlcControl1_Stopped(object sender,Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs e)
+		{
+			timer1.Enabled = false;
+		}
+
+		private void vlcControl1_PositionChanged(object sender,Vlc.DotNet.Core.VlcMediaPlayerPositionChangedEventArgs e)
+		{
+			if(!lockTrackbar)
+			{
+				Invoke(new Action(delegate
+				{
+					trackBar1.Value = Math.Min(trackBar1.Maximum,(int)(current_offset + vlcControl1.Position * current_length));
+				}));
+			}
+		}
+
+		private void vlcControl1_VlcLibDirectoryNeeded(object sender,Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs e)
+		{
+			e.VlcLibDirectory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(),"./VLC/"));
+			if(!e.VlcLibDirectory.Exists)
+			{
+				FolderBrowserDialog dialog = new FolderBrowserDialog();
+				dialog.Description = "Select Vlc libraries folder.\nSuch as your VLC installation path.";
+				dialog.RootFolder = Environment.SpecialFolder.Desktop;
+				if(dialog.ShowDialog() == DialogResult.OK)
+				{
+					e.VlcLibDirectory = new DirectoryInfo(dialog.SelectedPath);
+				}
+				else
+				{
+					MessageBox.Show("Vlc libraries not found,exiting...","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+					Environment.Exit(-1);
+				}
+			}
+		}
+
+		private void button1_Click(object sender,EventArgs e)
+		{
+			vlcControl1.Position = 0.99f;
+		}
+	}
+}
